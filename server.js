fbe6673f8cb148cd3ec5f95aeda0480c60a4c7fa
Akewushola abdulbakri temitope <neaterry6@gmail.com>
2025-04-27 02:24:28 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,41 @@
+const express = require("express");
+const fs = require("fs");
 const { pickupLines, rizzLines } = require("./pickupLines");
 
+const app = express();
+app.use(express.json());
+
+const allLines = [...pickupLines, ...rizzLines];
+
+// Get a random pickup or rizz line
 app.get("/random", (req, res) => {
-    const allLines = [...pickupLines, ...rizzLines];
     const randomIndex = Math.floor(Math.random() * allLines.length);
     res.json({ line: allLines[randomIndex] });
-})
+});
+
+// Get all pickup and rizz lines
+app.get("/all", (req, res) => {
+    res.json({ pickupLines, rizzLines });
+});
+
+// Add a new pickup or rizz line
+app.post("/add", (req, res) => {
+    const { type, line } = req.body;
+    if (!line || (type !== "pickup" && type !== "rizz")) {
+        return res.status(400).json({ error: "Invalid request. Provide type ('pickup' or 'rizz') and a line." });
+    }
+
+    if (type === "pickup") {
+        pickupLines.push(line);
+    } else {
+        rizzLines.push(line);
+    }
+
+    fs.writeFileSync("./pickupLines.js", `module.exports = { pickupLines: ${JSON.stringify(pickupLines)}, rizzLines: ${JSON.stringify(rizzLines)} };`);
+
+    res.json({ message: "Pickup/rizz line added!", type, line });
+});
+
+// Start the server
+const PORT = 3000;
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
